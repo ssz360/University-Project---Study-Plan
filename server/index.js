6d38a21e411b40e7f8f930f62b309ba41dbec356
SSZ <barg3abz@gmail.com>
2022-06-01 20:08:08 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+app.use(cors())
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

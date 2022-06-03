4a69550361a5f2976db512cf115c03164c0dd9ce
SSZ <barg3abz@gmail.com>
2022-06-03 17:49:31 +0200

    add tooltip

--- client/src/components/studyPlan/studyPlanTableElement/studyPlanTableElement.component.js
+++ client/src/components/studyPlan/studyPlanTableElement/studyPlanTableElement.component.js
@@ -1,6 +1,19 @@
-import ReactTooltip from 'react-tooltip';
+import { useEffect } from 'react';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
 function StudyPlanTableElement() {
+
+    useEffect(() => {
+        tippy('.tooltip-span', {
+            content(reference) {
+              const content = reference.getAttribute('data-template');
+              return content;
+            }
+          });
+    }, [])
+
+
     return (
         <tr className="text-left">
             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -34,16 +47,17 @@ function StudyPlanTableElement() {
                     className="relative inline-block px-3 py-1 font-semibold text-gray-900 leading-tight cursor-pointer">
                     <span aria-hidden
                         className="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
-                    <span className="relative">Compatibility</span>
+                    <span className="relative tooltip-span" data-template="04GSPOV">Compatibility</span>
                 </span>
                 <span
                     className="relative inline-block px-3 py-1 font-semibold text-gray-900 leading-tight cursor-pointer">
                     <span aria-hidden
                         className="absolute inset-0 bg-yellow-200 opacity-50 rounded-full"></span>
-                    <span className="relative">Preparatory</span>
+                    <span className="relative tooltip-span" data-template="02GOLOV">Preparatory</span>
                 </span>
             </td>
         </tr>
+
     );
 }
 
